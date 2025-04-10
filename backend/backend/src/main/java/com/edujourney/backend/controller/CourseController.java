3f4b37b50c1dfcdadf19cfb1afdb320efe5efea4
Thanuja Nipun <thanujanipun6@gmail.com>
2025-04-10 20:29:41 +0530

    connected backend and frontend in jobs function and added header to job page # Set route app js # added axios and mui library for frontend

--- backend/backend/src/main/java/com/edujourney/backend/controller/CourseController.java
+++ backend/backend/src/main/java/com/edujourney/backend/controller/CourseController.java
@@ -1,15 +1,21 @@
 package com.edujourney.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edujourney.backend.model.Course;
 import com.edujourney.backend.repository.CourseRepository;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 
 public class CourseController {
     
@@ -21,6 +27,11 @@ public class CourseController {
     Course newCourse (@RequestBody Course newCourse) {
         return courseRepository.save(newCourse);
 
+    }
+
+    @GetMapping("/api/course")
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
     }
     
 }
--- backend/backend/src/main/java/com/edujourney/backend/controller/JobController.java
+++ backend/backend/src/main/java/com/edujourney/backend/controller/JobController.java
@@ -1,12 +1,17 @@
 package com.edujourney.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edujourney.backend.model.Job;
 import com.edujourney.backend.repository.JobRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -19,6 +24,11 @@ public class JobController {
     @PostMapping("/api/job")
     Job newJob (@RequestBody Job newJob) {
         return jobRepository.save(newJob);
+    }
+
+    @GetMapping("/api/job")
+    public List<Job> getAllJobs() {
+        return jobRepository.findAll();
     }
     
 }
