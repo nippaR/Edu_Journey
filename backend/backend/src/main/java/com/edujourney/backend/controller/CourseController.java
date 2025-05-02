8ae856119d4ad7c9ee95f0a59f21f4dd40ceccd5
Nisansala Rasanjali <IT22547774@my.sliit.lk>
2025-05-02 23:18:44 +0530

    Update CourseController.java

--- backend/backend/src/main/java/com/edujourney/backend/controller/CourseController.java
+++ backend/backend/src/main/java/com/edujourney/backend/controller/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import com.edujourney.backend.model.Course;
 import com.edujourney.backend.repository.CourseRepository;
 
