2cc1132c59994da2dbc8312132ee21681ab56c7b
Ashen Mayuranga <ashen.ayuranga@gmail.com>
2025-05-02 18:19:02 +0530

    add Track for Postcontroller

--- backend/backend/src/main/java/com/edujourney/backend/controller/PostController.java
+++ backend/backend/src/main/java/com/edujourney/backend/controller/PostController.java
@@ -1,11 +1,15 @@
 package com.edujourney.backend.controller;
 
 import com.edujourney.backend.model.Post;
+import com.edujourney.backend.model.Notification;
 import com.edujourney.backend.repository.PostRepository;
+import com.edujourney.backend.repository.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal; 
 import java.util.List;
 
 @RestController
@@ -15,10 +19,33 @@ public class PostController {
     @Autowired
     private PostRepository postRepository;
 
+       // Auto-trigger notifications
+    @Autowired
+    private NotificationRepository notificationRepository;
+
     // 1. Create a new post
     @PostMapping
-    public Post createPost(@RequestBody Post post) {
-        return postRepository.save(post);
+    //public Post createPost(@RequestBody Post post) {
+    //    return postRepository.save(post);
+       public ResponseEntity<Post> createPost(
+       @RequestBody Post post,
+       Principal principal
+   ) {
+       // 1) Save the post
+       Post savedPost = postRepository.save(post);
+
+       // 2) Create a notification for the current user
+       String userEmail = principal.getName();
+       String msg = "Your new post: \"" + post.getDescription() + "\" has been created.";
+      // Notification note = new Notification(userEmail, msg);
+       //notificationRepository.save(note);
+
+       Notification note = new Notification(userEmail, msg);
+       Notification savedNote = notificationRepository.save(note);
+       System.out.println("Saved notification id=" + savedNote.getId()
+       + " message=\"" + savedNote.getMessage() + "\" for user=" + userEmail);
+
+       return new ResponseEntity<>(savedPost, HttpStatus.CREATED);
     }
 
     // 2. Read all posts
