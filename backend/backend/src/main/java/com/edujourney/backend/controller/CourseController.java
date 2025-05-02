607274e00a7e487ad9a621889f29646194d8d7bd
Nisansala Rasanjali <IT22547774@my.sliit.lk>
2025-05-02 23:19:49 +0530

    Update CourseController.java

--- backend/backend/src/main/java/com/edujourney/backend/controller/CourseController.java
+++ backend/backend/src/main/java/com/edujourney/backend/controller/CourseController.java
@@ -11,6 +11,7 @@ import com.edujourney.backend.repository.CourseRepository;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -21,17 +22,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class CourseController {
     
     @Autowired
+    
     private CourseRepository courseRepository;
     
     @PostMapping("/api/course")
     
+    
     Course newCourse (@RequestBody Course newCourse) {
+        
         return courseRepository.save(newCourse);
 
+        
     }
 
     @GetMapping("/api/course")
+    
     public List<Course> getAllCourses() {
+        
         return courseRepository.findAll();
     }
     
