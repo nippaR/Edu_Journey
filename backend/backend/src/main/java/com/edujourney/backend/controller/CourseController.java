a03cff9e0817222bf3957d03b199b3c206519947
Thanuja Nipun <thanujanipun6@gmail.com>
2025-05-03 23:27:01 +0530

    Added put mapping and Delete Mapping for backend

--- backend/backend/src/main/java/com/edujourney/backend/controller/CourseController.java
+++ backend/backend/src/main/java/com/edujourney/backend/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.edujourney.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -10,29 +11,68 @@ import com.edujourney.backend.model.Course;
 import com.edujourney.backend.repository.CourseRepository;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
 @CrossOrigin("http://localhost:3000")
+@RequestMapping("/api")
 
 public class CourseController {
     
     @Autowired
     private CourseRepository courseRepository;
     
-    @PostMapping("/api/course")
+    @PostMapping("/course")
     
     Course newCourse (@RequestBody Course newCourse) {
         return courseRepository.save(newCourse);
 
     }
 
-    @GetMapping("/api/course")
+    @GetMapping("/course")
     public List<Course> getAllCourses() {
         return courseRepository.findAll();
     }
     
+    
+    @GetMapping("/course/{id}")
+    public ResponseEntity<Course> getCourseById(@PathVariable String id) {
+        return courseRepository.findById(id)
+                .map(course -> ResponseEntity.ok(course))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/course/{id}")
+    public String updateCourse(@PathVariable String id, @RequestBody Course updateCourse) {
+        
+        return courseRepository.findById(id)
+                .map(course -> {
+                    course.setTitle(updateCourse.getTitle());
+                    course.setDescription(updateCourse.getDescription());
+                    course.setInstructor(updateCourse.getInstructor());
+                    course.setDuration(updateCourse.getDuration());
+                    course.setLevel(updateCourse.getLevel());
+                    course.setKeywords(updateCourse.getKeywords());
+                    return ResponseEntity.ok(courseRepository.save(course));
+                })
+                .orElse(ResponseEntity.notFound().build()).toString();
+    }
+
+    @DeleteMapping("/course/{id}")
+    public ResponseEntity<Void> deleteCourse(@PathVariable String id) {
+        return courseRepository.findById(id)
+                .map(course -> {
+                    courseRepository.delete(course);
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
--- backend/backend/src/main/java/com/edujourney/backend/controller/JobController.java
+++ backend/backend/src/main/java/com/edujourney/backend/controller/JobController.java
@@ -1,16 +1,21 @@
 package com.edujourney.backend.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edujourney.backend.model.Job;
 import com.edujourney.backend.repository.JobRepository;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 
@@ -31,5 +36,37 @@ public class JobController {
         return jobRepository.findAll();
     }
     
+    @GetMapping("/api/job/{id}")
+    public ResponseEntity<Job> getJobId(@PathVariable String id) {
+        return jobRepository.findById(id)
+            .map(job -> ResponseEntity.ok(job))
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/api/job/{id}")
+    public ResponseEntity<Job> updateJob(@PathVariable String id, @RequestBody Job updateJob) {
+            return jobRepository.findById(id)
+                .map(job -> {
+                    job.setTitle(updateJob.getTitle());
+                    job.setLocation(updateJob.getLocation());
+                    job.setCompany(updateJob.getCompany());
+                    job.setWorkplaceType(updateJob.getWorkplaceType());
+                    job.setJobType(updateJob.getJobType());
+                    job.setDescription(updateJob.getDescription());
+                    return ResponseEntity.ok(jobRepository.save(job));
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/api/job/{id}")
+    public ResponseEntity<Void> deleteJob(@PathVariable String id) {
+        return jobRepository.findById(id)
+            .map(job -> {
+                jobRepository.delete(job);
+                return ResponseEntity.noContent().<Void>build();
+            })
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    
 }
-// package com.edujourney.backend.controller;
