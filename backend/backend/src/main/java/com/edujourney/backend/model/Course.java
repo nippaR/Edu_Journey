14677f9697d762d9074dc538d388fd6fe02ae76e
Nisansala Rasanjali <IT22547774@my.sliit.lk>
2025-05-19 23:32:44 +0530

    Update Course.java

--- backend/backend/src/main/java/com/edujourney/backend/model/Course.java
+++ backend/backend/src/main/java/com/edujourney/backend/model/Course.java
@@ -1,6 +1,7 @@
 package com.edujourney.backend.model;
 
 import org.springframework.data.annotation.Id;
+
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "courses")
